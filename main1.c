17dda376946718d4b1e15c690a7c939dfca12fe6
Sachin8124 <burmansachin08@gmail.com>
2023-07-16 21:17:38 +0530

    #c
    learn and explore

---
+++ main1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int age;
+    printf("enter age : ");
+    scanf("%d", &age);
+
+    if(age > 18) {
+        printf("adult \n");
+    }
+    else if(age > 13 && age < 18) {
+        printf("teenager \n");
+    }
+    else {
+        printf("Child");
+    }
+    return 0;
+}
